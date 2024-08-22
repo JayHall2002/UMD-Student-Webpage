79bf202cddf248b0b9e15e4ae39547be8a465f4d
JayHall2002 <Ahallaj20002@gmail.com>
2024-08-22 13:32:52 -0500

    Added Stylistic changes to the home page. After this commit routing to other pages will be introduced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import umdlogo from './UMDLOGO.png';
 import './App.css';
-
+import dataanalystlogo from './Data-Analyst-Logo.jpg'
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={umdlogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div className="App-Subheader">
+          <h1>Welcome to UMD Information Science's Course Selector</h1>
+          </div>
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          onclick={() => window.open("https://www.testudo.umd.edu/", '_blank')}
           rel="noopener noreferrer"
         >
-          Learn React
+          Open UMD Testudo
+        </button>
+        <div className="Guidance-Instruction">
+        <h3>Click on Your Chosen Career Path</h3>
+        </div>
+        <div className="Data-Analyst">
+        <a href='https://www.coursera.org/articles/what-does-a-data-analyst-do-a-career-guide'
+         className='Data-Analyst-Interactive' 
+         target='_blank'
+         >
+          <img src={dataanalystlogo} alt='dataanalystlogo' />
         </a>
+        <p className="Data-Analyst-Caption">Data Analyst Career Path</p>
+        <header className='Data-Analyst-Courses'>
+          
+          <h4>View Recommended Courses</h4>
+        </header>
+        </div>
+        
       </header>
     </div>
+    
   );
 }
 
