ab01a51589f8c08710417bcb65200c966a6bea83
JayHall2002 <Ahallaj20002@gmail.com>
2024-08-22 17:02:01 -0500

    Correct Comments so that react does not display the text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import cyberseclogo from './Cyber-Security-Analyst.jpg'  // Image for the Cyber 
 
 function App() {
   return (
-    // This is the code for the top of the Homepage
-    <div className="App">  
+      
+    <div className="App">  {/* This is the code for the top of the Homepage */}
       <header className="App-header">
         <img src={umdlogo} className="App-logo" alt="logo" />
         <p>
@@ -14,7 +14,7 @@ function App() {
           <h1>Welcome to UMD Information Science's Course Selector</h1>
           </div>
         </p>
-        // The Circle that links to Testudo so students may refer to schedule of classes or register
+        {/* The Circle that links to Testudo so students may refer to schedule of classes or register */}
         <button
           className="App-link"
           onClick={() => window.open("https://www.testudo.umd.edu/", '_blank')}
@@ -22,11 +22,11 @@ function App() {
         >
           Open UMD Testudo
         </button>
-        // Nonfunctional Message to Instruct users to click on the Icons Below
+        {/* Nonfunctional Message to Instruct users to click on the Icons Below */}
         <div className="Guidance-Instruction">
         <h3>Click on Your Chosen Career Path</h3>
         </div>
-        // The image of a Data Analyst Hyperlinks to a credible source to explain what a Data Analyst is
+        /* The image of a Data Analyst Hyperlinks to a credible source to explain what a Data Analyst is */
         <div className="Data-Analyst">
         <a href='https://www.coursera.org/articles/what-does-a-data-analyst-do-a-career-guide'
          className='Data-Analyst-Interactive' 
@@ -41,7 +41,7 @@ function App() {
         </div>
         
       </header>
-      // Creating the CyberSecurity Logo and captions
+      {/* Creating the CyberSecurity Logo and captions */}
       <div className='Cyber-Security'>
         <a href='https://www.neit.edu/blog/what-does-a-cyber-security-analyst-do' className='Cyber-Security-Interactive' target='_blank'>
           <img src={cyberseclogo} alt='cyber-security' />
