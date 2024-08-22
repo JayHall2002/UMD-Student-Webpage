4a7062b7ca649fc9dd1809d5c925aee1aa9bd5f0
JayHall2002 <Ahallaj20002@gmail.com>
2024-08-22 16:49:15 -0500

    Initialized CyberSecuirty.js

---
+++ frontend/src/CyberSecurity.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CyberSecurity() {
+    return(
+        <h1>These are the recommended courses for the CyberSecurity Tract</h1>
+    );
+
+    
+}
+
+export default CyberSecurity;
